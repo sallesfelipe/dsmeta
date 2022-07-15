3286102e0067c0eae3585ea1e1ca291a98dfce00
Felipe <felipe.salles@outlook.com>
2022-07-15 18:15:14 -0300

    Heroku Sms

--- backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
@@ -36,9 +36,8 @@ public class SmsService {
 
 		String date = sale.getDate().getMonthValue() + "/" + sale.getDate().getYear();
 
-		String msg = "O vendedor " + sale.getSellerName() + " foi destaque em " + date + " com um total de R$ "
-				+ new DecimalFormat("#,##0.00").format(sale.getAmount());
-
+		String msg = String.format("O vendedor %s foi destaque em %s com um total de R$ %.2f", sale.getSellerName(),
+				date, sale.getAmount());
 		Twilio.init(twilioSid, twilioKey);
 
 		PhoneNumber to = new PhoneNumber(twilioPhoneTo);
